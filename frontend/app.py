ab9d0f252f07eb238127c1990200d0dcc1701e64
ubernex <gdloparco@gmail.com>
2020-08-10 22:25:41 -0400

    Added basic async request struct from JS to Python

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 app = Flask(__name__,
     static_folder="js",
     template_folder="client")
@@ -9,9 +9,16 @@ app = Flask(__name__,
 def mainpage():
     return render_template("index.html")
 
-@app.route('/<name>')
-def user(name):
-    return render_template("index.html", content=name)
+@app.route("/example_req", methods=['POST'])
+def example_func():
+    # Make sure no one is calling through their browser
+    if request.method == 'POST':
+        # Make a dict structure to hold fake data to pass to JS
+
+
+        return jsonify('hello or what ever variable or text you put here get sent back to your javascript')
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
