a6b6c021a7c21ee06c0e30726bf9e3ad98e685b5
jaymody <jaykmody@gmail.com>
2020-08-24 18:32:34 -0400

    read from predictions json instead of csv

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,6 @@
-import csv
-import math
-import os
-from random import random
+import json
 
-import numpy as np
-
-from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request
 
 # Main flask init
 app = Flask(__name__, static_folder="js", template_folder="client")
@@ -18,73 +13,16 @@ def mainpage():
 
 
 # COVID case data function (predictions and past data)
-@app.route("/casedata", methods=["POST"])
+@app.route("/casedata", methods=["POST", "GET"])
 def getCaseData():
     # Make sure no one is calling through their browser
+    with open("../prediction/temp/covidlocpreds.json") as fi:
+        data = json.load(fi)
+
     if request.method == "POST":
-        # Make a dict structure to hold fake data to pass to JS
-        data = []
-
-        # Data should have the following values:
-        #   Longitude (x)
-        #   Latitude (y)
-        #   # of cases (value)
-        #   Date
-
-        # Generate fake data for the time being
-        for i in range(100):
-            val = {
-                "x": random() * 100,
-                "y": random() * 100,
-                "value": math.floor(random() * 100),
-            }
-
-            data.append(val)
-
-        return jsonify(process_csv(save_location))
-        #return jsonify(data)
+        return jsonify(data)
     else:
-        return None
-
-
-# Save location for csv file
-save_location = os.path.join(os.getcwd(), "conposcovidloc.csv")
-
-
-def process_csv(save_location):
-    cr = csv.reader(open(save_location, "r"))
-    next(cr)
-    # Pull relevant data from the CSV in tuple form
-    formatted = []
-    for i, v in enumerate(cr):
-        formatted.append((v[2], v[15], v[16], v[12]))
-
-    # Create empty numpy array and fill with tuples from previous step
-    np_arr = np.empty((len(formatted),), dtype=object)
-    for i, v in enumerate(formatted):
-        np_arr[i] = v
-
-    # Create array of unique tuples and their counts
-    unique_elements, counts_elements = np.unique(np_arr, return_counts=True)
-
-    # Convert to dict containing dates and list of tuples containing location and case counts
-    complete_arr = {}
-
-    for i, v in enumerate(unique_elements):
-        # Make an obj to store in the dict
-        val = {
-            "x": float(v[2]),
-            "y": float(v[1]),
-            "value": int(counts_elements[i]),
-            "loc": v[3]
-        }
-
-        if v[0] in complete_arr:
-            complete_arr[v[0]].append(val)
-        else:
-            complete_arr[v[0]] = [val]
-
-    return complete_arr
+        return jsonify(data)
 
 
 # To launch the flask app on runtime
